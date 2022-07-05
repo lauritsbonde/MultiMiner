0160536dc164a8be3218271e2c237a050026dca9
lauritsbonde <lauritsbonde@hotmail.dk>
2022-07-06 01:15:22 +0200

    test

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+console.log(process.env.REACT_APP_AUTH0_DOMAIN);
+
 root.render(
 	<React.StrictMode>
 		<Auth0Provider domain={`${process.env.REACT_APP_AUTH0_DOMAIN}`} clientId={`${process.env.REACT_APP_AUTH0_CLIENT_ID}`} redirectUri={window.location.origin}>
