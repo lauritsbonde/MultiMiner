56d13e5b472851935ac3abda59bd9fc7c75607e7
lauritsbonde <lauritsbonde@hotmail.dk>
2023-03-16 16:45:00 +0100

    added line to display env

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Typography } from '@mui/material';
 
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<UserProvider>
+			{process.env.NEXT_PUBLIC_ENVIRONMENT !== 'production' && (
+				<Typography variant="body1" sx={{ width: '100vw', height: '12px', fontSize: '12px', textAlign: 'center', backgroundColor: 'red' }}>
+					env: {process.env.NEXT_PUBLIC_ENVIRONMENT}
+				</Typography>
+			)}
 			<Component {...pageProps} />
 		</UserProvider>
 	);
