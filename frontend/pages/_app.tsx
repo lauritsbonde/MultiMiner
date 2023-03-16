32975a144b223892018e6686e0f4948cf964c8b4
lauritsbonde <lauritsbonde@hotmail.dk>
2023-03-16 16:54:16 +0100

    removed docker image caching

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { Typography } from '@mui/material';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<UserProvider>
 			{process.env.NEXT_PUBLIC_ENVIRONMENT !== 'production' && (
-				<Typography variant="body1" sx={{ width: '100vw', height: '12px', fontSize: '12px', textAlign: 'center', backgroundColor: 'red' }}>
+				<Typography variant="body1" sx={{ width: '100vw', height: '18px', fontSize: '12px', textAlign: 'center', backgroundColor: 'red', margin: 0, padding: 0 }}>
 					env: {process.env.NEXT_PUBLIC_ENVIRONMENT}
 				</Typography>
 			)}
